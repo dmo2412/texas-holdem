d2d668f13338d4b4f668d4efe34fa989d067c919
Danny Morgan <djmorgan2412@gmail.com>
2020-06-24 13:43:01 -0400

    Finish logic for playing game without betting and rendered into html

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,12 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
-    // const cards = document.querySelector('.cards');
-    const playercards = document.querySelector('playercards');
-    const flop = document.querySelector('flop');
-    const turn = document.querySelector('turn');
-    const river = document.querySelector('river');
-    const victor = document.querySelector('victor');
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
     const suits = ['s', 'c', 'h', 'd'];
     let cards = [];
@@ -45,52 +39,87 @@ document.addEventListener("DOMContentLoaded", () => {
         cards[i] = cards[j];
         cards[j] = temp;
     };
-    document.getElementById('playercards').onclick = function dealCards() {
+    document.getElementById('hands').onclick = function dealCards() {
         for (let i = 0; i < 2; i++) {
-            player1.cardPool.push(cards[0]);
             player1.holecards.push(cards[0]);
+            player1.cardPool.push(cards[0]);
             cards.shift()
-            player2.cardPool.push(cards[0]);
             player2.holecards.push(cards[0]);
+            player2.cardPool.push(cards[0]);
             cards.shift()
-            player3.cardPool.push(cards[0]);
             player3.holecards.push(cards[0]);
+            player3.cardPool.push(cards[0]);
             cards.shift()
         }
         let arr = [player1.holecards, player2.holecards, player3.holecards];
         console.log(arr)
-        // debugger
-        // playercards.innerHTML = '1';
-        // debugger
+        hands.innerHTML = arr;
+        // let middlecards = cards.slice(0, 5);
+        // for (let i = 0; i < middlecards.length; i++) {
+        //     player1.cardPool.push(middlecards[i])
+        //     player2.cardPool.push(middlecards[i])
+        //     player3.cardPool.push(middlecards[i])
+        // }
+        // let first3 = middlecards.slice(0,3);
+        // let fourth = middlecards[3];
+        // let fifth = middlecards[4];
+        // let housecards = [middlecards, first3, fourth, fifth]
+        // return housecards
     }
+    let middlecards = [];
 
-    let middlecards = cards.slice(0, 5);
-
-    for (let i = 0; i < middlecards.length; i++) {
-        player1.cardPool.push(middlecards[i])
-        player2.cardPool.push(middlecards[i])
-        player3.cardPool.push(middlecards[i])
-    }
-
-    let first3 = middlecards.slice(0,3);
-    let fourth = middlecards[3];
-    let fifth = middlecards[4];
-    let hand1 = Hand.solve(player1.cardPool);
-    let hand2 = Hand.solve(player2.cardPool);
-    let hand3 = Hand.solve(player3.cardPool);
-    let hands = [hand1, hand2, hand3];
-    // playerCards.append(hands)
-    let winner = Hand.winners(hands);
-    let champ;
-    if (hand1.cardPool === winner[0].cardPool) {
-        champ = player1
-    } else if (hand2.cardPool === winner[0].cardPool) {
-        champ = player2
-    } else {
-        champ = player3
+    document.getElementById('flop').onclick = function flopCards() {
+        c = cards.slice(0,3)
+        middlecards = middlecards.concat(c);
+        player1.cardPool = player1.cardPool.concat(middlecards)
+        player2.cardPool = player2.cardPool.concat(middlecards)
+        player3.cardPool = player3.cardPool.concat(middlecards)
+        flop.innerHTML = middlecards;
+        cards = cards.slice(3,46)
     }
 
 
-    console.log(champ)
-    debugger  
+
+    document.getElementById('turn').onclick = function turnCard() {
+        middlecards.push(cards[0]);
+        player1.cardPool.push(middlecards[3]);
+        player2.cardPool.push(middlecards[3]);
+        player3.cardPool.push(middlecards[3]);
+        turn.innerHTML = player1.cardPool[5];
+        cards.shift();
+    }
+
+    document.getElementById('river').onclick = function riverCard() {
+        middlecards.push(cards[0]);
+        player1.cardPool.push(middlecards[4]);
+        player2.cardPool.push(middlecards[4]);
+        player3.cardPool.push(middlecards[4]);
+        river.innerHTML = player1.cardPool[6];
+        cards.shift();
+    }
+
+    document.getElementById('solve').onclick = function winningHand() {
+        let hand1 = Hand.solve(player1.cardPool);
+        let hand2 = Hand.solve(player2.cardPool);
+        let hand3 = Hand.solve(player3.cardPool);
+        let hands = [hand1, hand2, hand3];
+        let winner = Hand.winners(hands);
+        let champ;
+        if (hand1.cardPool === winner[0].cardPool) {
+            champ = player1
+        } else if (hand2.cardPool === winner[0].cardPool) {
+            champ = player2
+        } else {
+            champ = player3
+        }
+        solve.innerHTML = champ.name;
+    }
+
+
+
+    
+    
+
+// // playerCards.append(hands)
+    
 })
